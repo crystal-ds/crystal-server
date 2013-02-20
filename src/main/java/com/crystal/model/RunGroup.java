95a2026cefc76ca4845e334a3f8ba8ff09fad557
Tristan Lewis <tmlewis@mitre.org>
2013-02-20 11:03:45 -0500

    added rungroup amoung others

--- src/main/java/com/crystal/model/RunGroup.java
+++ src/main/java/com/crystal/model/RunGroup.java
@@ -3,10 +3,33 @@
  */
 package com.crystal.model;
 
+import java.util.List;
+
 /**
  * @author tmlewis
  *
  */
 public class RunGroup {
 
+	private long id;
+	private List <run> runs;
+	public RunGroup(long id, List<run> runs) {
+		super();
+		this.id = id;
+		this.runs = runs;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public List<run> getRuns() {
+		return runs;
+	}
+	public void setRuns(List<run> runs) {
+		this.runs = runs;
+	}
+	
+	
 }
