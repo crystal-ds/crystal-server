83968e2a708ed91fa4f4a3157839b27d37c11bfd
Tristan Lewis <tmlewis@mitre.org>
2013-04-05 11:38:15 -0400

    noOp model

---
+++ src/main/java/org/mitre/crystal/model/instance/NoOpModelRunner.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.mitre.crystal.model.instance;
+
+import org.mitre.crystal.model.ModelRunInstance;
+import org.mitre.crystal.model.ModelSpecification;
+
+/**
+ * @author tmlewis
+ *
+ */
+public class NoOpModelRunner extends ModelSpecification {
+
+	/**
+	 * 
+	 */
+	public NoOpModelRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mitre.crystal.model.ModelSpecification#runModel(org.mitre.crystal.model.ModelRunInstance)
+	 */
+	@Override
+	public void runModel(ModelRunInstance mri) {
+		// No Ops Allowed
+	}
+
+}
