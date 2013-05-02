04855da13d700a7809d18e65d141766772f5ca47
yikliu <yikliu@umail.iu.edu>
2013-05-02 16:40:06 -0400

    Capitalize files names in /web/view/

---
+++ src/main/java/org/mitre/crystal/web/view/BatchJobIdView.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.mitre.crystal.web.view;
+
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.impl.DefaultPrettyPrinter;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.mitre.crystal.model.BatchJob;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+/**
+ * @author tmlewis
+ *
+ */
+@Component("batchJobIdView")
+public class BatchJobIdView extends AbstractView{
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.view.AbstractView#renderMergedOutputModel(java.util.Map, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		
+		
+		BatchJob batchJob = (BatchJob) model.get("batchJob");
+		if(batchJob == null){
+			response.setStatus(HttpStatus.NOT_FOUND.value());
+		}
+		else{
+			response.setStatus(HttpStatus.OK.value());
+			response.setContentType("application/json");
+			
+			PrintWriter writer = response.getWriter();
+			ObjectMapper mapper = new ObjectMapper();
+			
+			JsonGenerator jsonGenerator = mapper.getJsonFactory().createJsonGenerator(writer);
+			jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter());
+			
+			jsonGenerator.writeStartObject();
+			
+			jsonGenerator.writeStringField("batchJob",	 batchJob.getId().toString());
+			jsonGenerator.writeEndObject();
+			jsonGenerator.flush();
+		}
+	}
+}
