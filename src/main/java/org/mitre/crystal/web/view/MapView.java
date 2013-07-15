0714f52b2af8d7843c95bccb5022126efd102daf
Tristan Lewis <tmlewis@mitre.org>
2013-07-15 13:58:16 -0400

    changed frome returning map, to returning 2 lists since you can't key off an object

--- src/main/java/org/mitre/crystal/web/view/MapView.java
+++ src/main/java/org/mitre/crystal/web/view/MapView.java
@@ -5,6 +5,7 @@ package org.mitre.crystal.web.view;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.impl.DefaultPrettyPrinter;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.hsqldb.lib.HashMap;
 import org.mitre.crystal.model.ModelRunInstance;
@@ -44,26 +46,19 @@ public class MapView extends AbstractView {
 			response.setContentType("application/json");
 			
 			ObjectMapper mapper = new ObjectMapper();
-			Set<ModelRunInstance> s = m.keySet();
-			HashMap myMap = new HashMap();
-////			
-////			
-			for (ModelRunInstance modelRunInstance : s) {
-				String mriString = mapper.writeValueAsString(modelRunInstance);
-				ScoreRunInstance sri = m.get(modelRunInstance);
-				myMap.put(mriString, sri);
-			}
-//			//String json = mapper.writeValueAsString(m);
-//			PrintWriter w = response.getWriter();
-//			w.print(mapper.writeValueAsString(m));
-//			w.flush();
 			
-			PrintWriter w = response.getWriter();
-			JsonGenerator jsonGenerator = mapper.getJsonFactory().createJsonGenerator(w);
+			Set<ModelRunInstance> mriSet = m.keySet();
+			Collection<ScoreRunInstance> sriSet = m.values();
+			
+			PrintWriter writer = response.getWriter();
+			JsonGenerator jsonGenerator = mapper.getJsonFactory().createJsonGenerator(writer);
+			jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter());
+			
 			jsonGenerator.writeStartObject();
-			jsonGenerator.writeFieldName("map");
-			jsonGenerator.writeObject(myMap);
-			jsonGenerator.writeEndObject();
+			jsonGenerator.writeFieldName("ModelRunInstance");
+			jsonGenerator.writeObject(mriSet);
+			jsonGenerator.writeFieldName("ScoreRunInstance");
+			jsonGenerator.writeObject(sriSet);
 			
 		}
 		
