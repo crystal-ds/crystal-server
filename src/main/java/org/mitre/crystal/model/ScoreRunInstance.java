24ebbb7513324730c9dbabee4f136ee51b79aefa
Tristan Lewis <tmlewis@mitre.org>
2013-08-28 18:55:23 -0400

    clean up of ScoreRunInstance

--- src/main/java/org/mitre/crystal/model/ScoreRunInstance.java
+++ src/main/java/org/mitre/crystal/model/ScoreRunInstance.java
@@ -3,7 +3,6 @@
  */
 package org.mitre.crystal.model;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
@@ -24,44 +23,47 @@ import javax.persistence.TemporalType;
 
 /**
  * @author tmlewis
- *
+ *ScoreingRunInstance is the Scoreing Model equilvent of a Model Run instance. The exception
+ *being that it does not have a list of inputs as all Scoreing Models are scored the same
+ *way
  */
 @Entity
 @Table(name="scoring_run_instance")
 public class ScoreRunInstance {
 
-	
-	/**
-	 * 
-	 */
-	
+
+	//Unique ID for this scoreing run
 	private long scoreRunInstanceID;
+	//the ID of the ModelRunInstance you are scoreing
 	private long mriJobInstanceID;
+	//The score(s) that the score engine generated
 	private Map <String,String> outputValues;
+	//The time it was scored
 	private Date timestamp;
-	
+
 	public ScoreRunInstance() {
 		setTimestamp(new Date());
 	}
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	public long getScoreRunInstanceID() {
 		return scoreRunInstanceID;
 	}
+	
 	public void setScoreRunInstanceID(long scoreRunInstanceID) {
 		this.scoreRunInstanceID = scoreRunInstanceID;
 	}
-	
+
 	@Column(name="mri_link")
 	public long getMriJobInstanceID() {
 		return mriJobInstanceID;
 	}
 	public void setMriJobInstanceID(long batchJobInstanceID) {
-		this.mriJobInstanceID = batchJobInstanceID;
+		mriJobInstanceID = batchJobInstanceID;
 	}
-	
+
 	@Column(name="output_json_value")
 	@ElementCollection(fetch=FetchType.EAGER)
 	@MapKeyColumn(name="output_name")
@@ -72,17 +74,18 @@ public class ScoreRunInstance {
 	public Map<String, String> getOutputValues() {
 		return outputValues;
 	}
+	
 	public void setOutputValues(Map<String, String> outputValues) {
 		this.outputValues = outputValues;
 	}
-	
+
 	@Basic
 	@Column(name="time_stamp")
 	@Temporal(TemporalType.TIMESTAMP)
 	public Date getTimestamp() {
 		return timestamp;
 	}
-	
+
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
