97b4e889c64d3522f9d10abb0e24de3ce43e10a4
Tristan Lewis <tmlewis@mitre.org>
2013-09-20 14:24:01 -0400

    Documentation

--- src/main/java/org/mitre/crystal/service/ScoringBatchJobService.java
+++ src/main/java/org/mitre/crystal/service/ScoringBatchJobService.java
@@ -10,30 +10,33 @@ import org.mitre.crystal.model.ScoringModelInput;
 
 /**
  * @author tmlewis
- *
+ * Manages Scoring Batch jobs
  */
 public interface ScoringBatchJobService {
 
 	/**
-	 * @param id
-	 * @return
+	 * @param id Id of the scoreing batch job you are looking for
+	 * @return The scoreing batch job
 	 */
 	public abstract SMBatchJob getScore(long id);
 
 	/**
-	 * @param vals
-	 * @param job
-	 * @param model 
-	 * @return
+	 * @param vals The inputs or rubric you want to score against
+	 * @param job The batch job you wish to score
+	 * @param model  The socring model you wish to use
+	 * @return The Scoreing Batch job created with the above information
 	 */
 	public abstract SMBatchJob score(ScoringModelInput vals, BatchJob job, ScoringModel model);
 
 	/**
-	 * @param id
-	 * @return
+	 * @param id The scoreing batch job you wish to return
+	 * @return The batch job requested
 	 */
 	public abstract SMBatchJob getSMBatchJob(long id);
-	
+	/**
+	 * 
+	 * @param id The SM batchjob you want to remove
+	 */
 	public abstract void deleteSMBatchJob(long id);
 
 }
