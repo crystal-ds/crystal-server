786ca7592de9ab1280d90d0de1b214def74c1acc
Tristan Lewis <tmlewis@mitre.org>
2013-09-20 18:31:13 -0400

    Documentation

--- src/main/java/org/mitre/crystal/service/WorkSpaceService.java
+++ src/main/java/org/mitre/crystal/service/WorkSpaceService.java
@@ -10,15 +10,51 @@ import org.mitre.crystal.model.WorkSpace;
 
 /**
  * @author tmlewis
- *
+ * Provide CRUD operations for workspaces
  */
 public interface WorkSpaceService {
 	
+	/**
+	 * 
+	 * @param batchJobID The batchjob that the works space will be created
+	 * @return The created workspace
+	 */
 	public abstract WorkSpace createWorkSpace(Long batchJobID);
+	
+	/**
+	 * 
+	 * @param ID The ID of the workspace you want
+	 * @return The requested workspace
+	 */
 	public abstract WorkSpace getWorkSpace(Long ID);
+	
+	/**
+	 * 
+	 * @param workSpaceId The workspace ID to be updated
+	 * @param mask The mask of Model runs you wish to turn off
+	 * @return The updated workspace
+	 */
 	public abstract WorkSpace updateWorkSpace(Long workSpaceId, List<ModelRunInstance> mask);
+	
+	/**
+	 * 
+	 * @param workSpaceId The workspace ID to be updated
+	 * @param mask The list of Longs that represent model runs
+	 * @return Updated workspace
+	 */
 	public abstract WorkSpace updateWorkSpaceWithLong(Long workSpaceId, List<Long> mask);
+	/**
+	 * 
+	 * @param ws The workspace to restore
+	 * @return The updated workspace
+	 */
 	public abstract WorkSpace restoreWorkSpace(WorkSpace ws);
+	
+	
+	/**
+	 * 
+	 * @param ws The workspace to remove
+	 */
 	public abstract void deleteWorkSpace(WorkSpace ws);
 
 }
