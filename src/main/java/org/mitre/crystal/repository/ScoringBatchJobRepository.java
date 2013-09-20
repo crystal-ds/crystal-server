a2bd06481901c3ca6b0a991cb89453a9f021e311
Tristan Lewis <tmlewis@mitre.org>
2013-09-20 13:50:31 -0400

    Documentation

--- src/main/java/org/mitre/crystal/repository/ScoringBatchJobRepository.java
+++ src/main/java/org/mitre/crystal/repository/ScoringBatchJobRepository.java
@@ -7,24 +7,24 @@ import org.mitre.crystal.model.SMBatchJob;
 
 /**
  * @author tmlewis
- *
+ * Interface that any ScoreingBatchJobRepository will need to implement
  */
 public interface ScoringBatchJobRepository {
 
 	/**
-	 * @param id
-	 * @return
+	 * @param id ID of the batchjob you are looking for
+	 * @return The Scoreing model batch job requested, otherwise null
 	 */
 	SMBatchJob getBatchJob(long id);
 
 	/**
-	 * @param score
-	 * @return 
+	 * @param score The SM batchJob you wish to save
+	 * @return The SMBatchJob that that has been saved, complete with ID
 	 */
 	SMBatchJob save(SMBatchJob score);
 
 	/**
-	 * @param id
+	 * @param id Removes this batchjob
 	 */
 	void deleteBatchJob(SMBatchJob job);
 
