e41bbfa26aabefce660c249d39c4723d4c501e1e
Tristan Lewis <tmlewis@mitre.org>
2013-09-20 13:49:54 -0400

    Documentation

--- src/main/java/org/mitre/crystal/repository/BatchJobRepository.java
+++ src/main/java/org/mitre/crystal/repository/BatchJobRepository.java
@@ -9,27 +9,32 @@ import org.mitre.crystal.model.BatchJob;
 
 /**
  * @author tmlewis
- *
+ * Interface for the BatchJobRepository. These are the methods that any BJ repo will need to support
+ * 
  */
 public interface BatchJobRepository {
 
 	/**
-	 * @param bj
-	 * @return
+	 * @param bj Batchjob to be saved
+	 * @return The saved batched job complete with ID
 	 */
 	BatchJob save(BatchJob bj);
 
 	/**
-	 * @param batchJobId
-	 * @return
+	 * @param batchJobId BatchJob ID that you want to reterive
+	 * @return The Batchjob you are looking for or null if not found
 	 */
 	BatchJob getBatchJob(long batchJobId);
 
 	/**
-	 * @param batchJob
+	 * @param batchJob Batchjob to remove. 
 	 */
 	void deleteBatchJob(BatchJob batchJob);
 	
+	/**
+	 * 
+	 * @return All batchjobs availabe regardless of status
+	 */
 	List<BatchJob> getAllBatchJobs();
 
 }
