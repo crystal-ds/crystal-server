5c8c56d1453ec1911e3e02a777c8619920184512
Tristan Lewis <tmlewis@mitre.org>
2013-09-20 14:23:45 -0400

    Documentation

--- src/main/java/org/mitre/crystal/service/ModelService.java
+++ src/main/java/org/mitre/crystal/service/ModelService.java
@@ -9,15 +9,31 @@ import org.mitre.crystal.model.RunnableModel;
 
 /**
  * @author tmlewis
- *
+ * CRUD operations for Runnable Models
  */
 public interface  ModelService {
-	
 
-	
+	/**
+	 * 
+	 * @param modelSpec Saves a model 
+	 * @return The saved model complete with ID
+	 */
 	public RunnableModel saveModel(RunnableModel modelSpec);
+	/**
+	 * Retrieves a model from the repo
+	 * @param id The ID of the model
+	 * @return The model
+	 */
 	public RunnableModel getModel(long id);
+	/**
+	 * Removes a model from the repo
+	 * @param modelSpec The model to remove
+	 */
 	public void removeModel(RunnableModel modelSpec);
+	/**
+	 * A list of all models
+	 * @return A list of all available models
+	 */
 	public List<RunnableModel> getAllModels();
-	
+
 }
