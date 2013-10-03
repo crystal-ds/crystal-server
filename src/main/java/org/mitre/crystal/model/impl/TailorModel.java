fbd6afaea22e399c0c0368bab9967133bf240c7e
mhowansky <mhowansky@mitre.org>
2013-10-03 11:07:36 -0400

    Changed Tailor Model Output

--- src/main/java/org/mitre/crystal/model/impl/TailorModel.java
+++ src/main/java/org/mitre/crystal/model/impl/TailorModel.java
@@ -6,6 +6,7 @@ package org.mitre.crystal.model.impl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import javax.annotation.PostConstruct;
 
@@ -14,16 +15,26 @@ import org.mitre.crystal.model.InputType;
 import org.mitre.crystal.model.ModelRunInstance;
 import org.mitre.crystal.model.ModelSpecification;
 import org.mitre.crystal.model.RunnableModel;
-import org.mitre.crystal.model.InputVariableType;
 
 /**
  * @author tmlewis
  * 
  */
 public class TailorModel extends RunnableModel {
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.mitre.crystal.model.ModelSpecification#run(org.mitre.crystal.model
+	 * .ModelRunInstance)
+	 */
+
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 63069294821569859L;
-	
+
 	public TailorModel() {
 		this.setModelSpec(new ModelSpecification());
 		this.setName("Tailor");
@@ -36,20 +47,26 @@ public class TailorModel extends RunnableModel {
 		//COST  =  100*units * (1.1 * isolated / BEACONS)
 		//CASUALTIES =   ((10 * ISOLATED)/BEACONS)   /UNITS 
 		//TIME Air = 1 Ground = 2 Sea = 3
+		Random generator = new Random();
 		Map<String, String> outputValues = mri.getOutputValues();
-			for (int i = 0; i < 10; i++) {
-				outputValues.put("Value" + i, Integer.toString(i));
+			for (int i = 0; i < 1; i++) {
+				int r = generator.nextInt();
+				outputValues.put("Value" + i, Integer.toString(r));
 			}
 			mri.setOutputValues(outputValues);
 	}
 
+	/**
+	 * 
+	 */
 	@PostConstruct
 	public void init() {
-		
+
+	
+
 		InputNode ipn1 = new InputNode();
 		ipn1.setName("Number of units to send");
 		ipn1.setType(InputType.INTEGER);
-		ipn1.setVariableType(InputVariableType.INITIAL_CONDITION);
 		Map<String, String> m1 = ipn1.getProperties();
 		m1.put("min", "0");
 		m1.put("max", "100");
@@ -60,7 +77,6 @@ public class TailorModel extends RunnableModel {
 		InputNode ipn2 = new InputNode();
 		ipn2.setName("Type of Recovery");
 		ipn2.setType(InputType.STRING);
-		ipn2.setVariableType(InputVariableType.INTERMEDIARY_VARIABLE);
 		Map<String, String> m = new HashMap<String, String>();
 		m.put("regex", "air|ground|sea");
 		m.put("value", "ground");
@@ -69,7 +85,6 @@ public class TailorModel extends RunnableModel {
 		InputNode ipn3 = new InputNode();
 		ipn3.setName("How many people are stranded");
 		ipn3.setType(InputType.INTEGER);
-		ipn3.setVariableType(InputVariableType.COURSE_OF_ACTION);
 		Map<String, String> m3 = new HashMap<String, String>();
 		m3.put("min", "0");
 		m3.put("max", "100");
@@ -82,7 +97,6 @@ public class TailorModel extends RunnableModel {
 		InputNode ipn4 = new InputNode();
 		ipn4.setName("How many beacons have locations");
 		ipn4.setType(InputType.INTEGER);
-		ipn4.setVariableType(InputVariableType.INTERMEDIARY_VARIABLE);
 		Map<String, String> m4 = new HashMap<String, String>();
 		m4.put("min", "0");
 		m4.put("max", "100");
@@ -96,5 +110,7 @@ public class TailorModel extends RunnableModel {
 		inputList.add(ipn3);
 		inputList.add(ipn4);
 		this.setInputs(inputList);
+
 	}
+
 }
